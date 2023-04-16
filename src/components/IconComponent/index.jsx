6b3ebf659bce4b731e57fb0831b7800ada02de88
ImranRovano <imranhuhn1@gmail.com>
2023-04-16 05:41:43 -0400

    removed comments

--- src/components/IconComponent/index.jsx
+++ src/components/IconComponent/index.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 export const ArrowIcon = () => {
   return (
     <svg
-      width={"20px"}
+      width="20px"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
